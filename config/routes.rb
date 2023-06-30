3111f08003ed2e60822fbb2eaf4e9678122d7588
Nathalie Moraga <129231844+NatieMor@users.noreply.github.com>
2023-06-29 22:06:26 -0400

    actualizacion de rutas con post

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
-  get 'documental_film/index'
+  get 'documental_film', to: 'documental_film#index'
   get 'documental_film/index'
   get 'documental_film/create'
   get 'documental_film/new'
+
   get 'serie_tv/index'
+  get 'serie_tv', to: 'serie_tv#index'
   get 'serie_tv/create'
   get 'serie_tv/new'
+
   get 'pelicula/index'
+  get 'pelicula', to: 'pelicula#index'
   get 'pelicula/create'
   get 'pelicula/new'
 
-  get 'documental_film', to: 'documental_film#index'
-  get 'serie_tv', to: 'serie_tv#index'
-  get 'pelicula', to: 'pelicula#index'
- 
+  post 'documental_film/create'
+  post 'serie_tv/create'
+  post 'pelicula/create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
